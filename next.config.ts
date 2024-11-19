1096e0fa8704865472420aa7359efd64586ee6c2
kaiba <kaibadash@gmail.com>
2024-11-19 20:13:59 +0900

    WIP

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/dummy_site',
-  assetPrefix: '/dummy_site/',
+  output: 'export' 
 };
 
 module.exports = nextConfig;
