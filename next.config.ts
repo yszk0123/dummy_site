74ecdfc9ab8a3a0bf297a9e89a1b18c2220e80e0
kaiba <kaibadash@gmail.com>
2024-11-19 21:34:53 +0900

    Generate compiled resources to dist

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/dummy_site'
+  basePath: '/dummy_site',
+  distDir: 'dist'
 };
 
 module.exports = nextConfig;
