a580ee3f45fbe08524354c99bba03d58bca2f777
kaiba <kaibadash@gmail.com>
2024-11-19 17:24:55 +0900

    Show input text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 'use client'
 import Image from "next/image";
 import Login from "./components/Login";
+import TextForm from "./components/TextForm";
+import { useState } from 'react';
+
 export default function Home() {
+  const [showTextForm, setShowTextForm] = useState(false);
+
   const handleLoginSuccess = () => {
     console.log('ログインが成功しました');
+    setShowTextForm(true);
   };
+
   return (
     <div>
       <main>
         <h1>Dummy Site</h1>
         <Login onLoginSuccess={handleLoginSuccess} />
+        {showTextForm && <TextForm />}
       </main>
     </div>
   );
